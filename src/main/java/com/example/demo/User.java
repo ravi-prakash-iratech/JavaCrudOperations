3699d9c1bfe2d43df10f1371bd8573f96f6e82f8
ravi <ravi.prakash@iratechnologies.com>
2024-07-09 18:01:38 +0530

    Second commit

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,2 +1,64 @@
-package com.example.demo;public class User {
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    //Constructor
+    public User(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Constructors, getters, and setters
+    public User() {}
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
+
--- src/main/java/com/example/demo/UserService.java
+++ src/main/java/com/example/demo/UserService.java
@@ -1,2 +1,38 @@
-package com.example.demo;public class UserService {
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        List<User>  itr =userRepository.findAll();
+
+        itr.forEach(u->{System.out.println("users ---> " + u);});
+        return userRepository.findAll();
+    }
+
+    public User getUserByEmail(String emailId) {
+        return userRepository.findByEmail(emailId);
+    }
+
+//    public User getUserByUserId(Long userId) {
+//        User x = userRepository.findUserByIdWithin(userId);
+//        System.out.println("getbyid" + x);
+//        return x;
+//    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(Long userId) {
+        userRepository.deleteById(userId);
+    }
 }
